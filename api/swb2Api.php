786083b33142331e981688ae59e6377c2f269dc7
Travis80 <woods49warhorse@hotmail.com>
2022-06-12 19:17:21 -0500

    First Commit

---
+++ api/swb2Api.php
@@ -0,0 +1,26 @@
+<?php
+	/*******************************************************************************************
+	Author: Travis Woods
+	License: GNU v2 Public
+	Description: This is basically a Star Wars Battlefront 2 (2005) rip off. The difference is 
+		obviously that this is not sponsered or affiliated with Star Wars in any way. Put more
+		legal info here.
+	*******************************************************************************************/
+	//this is here for quick copying
+	/*******************************************************************************************
+	*******************************************************************************************/
+
+	/*******************************************************************************************
+	The idea: Create a Star Wars battlefront rip off. But even that might be thinking too far
+	in advance if I can't get some basic game engine to work. I'm going to have to skip straight
+	from 2D graphics in canvas to WebGL. Its not that impossible because I did make a 3D rack.
+	I just wish I remembered more of the stuff I spent an insane amount of time learning in the
+	first place.
+
+	Some other concepts is running some kind of hash on the client code occasionally to make sure
+	it isn't being altered
+	*******************************************************************************************/
+
+	
+
+?>
